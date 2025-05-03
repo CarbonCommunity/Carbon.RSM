cf593c0d0720e5c5df3699ca27bf869818541caa
raul <raulssorban@gmail.com>
2025-05-04 00:44:15 +0200

    Fix for NetWrite NREs

--- src/RustServerMetrics/HarmonyPatches/NetWrite_Send_Patch.cs
+++ src/RustServerMetrics/HarmonyPatches/NetWrite_Send_Patch.cs
@@ -6,7 +6,7 @@ namespace RustServerMetrics.HarmonyPatches
     [HarmonyPatch(typeof(NetWrite), nameof(NetWrite.Send))]
     public class NetWrite_Send_Patch
     {
-        [HarmonyPostfix]
+        [HarmonyPrefix]
         public static void Postfix(NetWrite __instance, SendInfo info)
         {
             SingletonComponent<MetricsLogger>.Instance?.OnNetWriteSend(__instance, info);
