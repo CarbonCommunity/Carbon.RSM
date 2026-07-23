852885d9115ca330aacb1709cfe9966ebc7284a0
packs-packrat <packs-packrat@users.noreply.github.com>
2026-07-22 17:42:44 -0700

    Fix: stop overwriting Carbon's OnEntityKill hook via BaseNetworkable.Kill patch

---
+++ src/RustServerMetrics/HarmonyPatches/Delayed/BaseNetworkable_Kill_Metrics_Patch.cs
@@ -0,0 +1,52 @@
+using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+using Stopwatch = System.Diagnostics.Stopwatch;
+
+// ReSharper disable InconsistentNaming
+
+namespace RustServerMetrics.HarmonyPatches.Delayed;
+
+[DelayedHarmonyPatch]
+[HarmonyPatch]
+internal static class BaseNetworkable_Kill_Metrics_Patch
+{
+    private static readonly double TicksToMs = 1000.0 / Stopwatch.Frequency;
+
+    [HarmonyPrepare]
+    public static bool Prepare()
+    {
+        if (!RustServerMetricsLoader.__serverStarted)
+        {
+            Debug.Log("Note: Cannot patch BaseNetworkable_Kill_Metrics_Patch yet. We will patch it upon server start.");
+            return false;
+        }
+
+        return true;
+    }
+
+    [HarmonyTargetMethods]
+    public static IEnumerable<MethodBase> TargetMethods(Harmony harmonyInstance)
+    {
+        yield return AccessTools.Method(typeof(BaseNetworkable), nameof(BaseNetworkable.Kill));
+    }
+
+    [HarmonyPrefix]
+    public static void Prefix(ref long __state)
+    {
+        __state = Stopwatch.GetTimestamp();
+    }
+
+    [HarmonyPostfix]
+    public static void Postfix(long __state)
+    {
+        if (!MetricsLogger.IsReady)
+        {
+            return;
+        }
+
+        var ms = (Stopwatch.GetTimestamp() - __state) * TicksToMs;
+        MetricsLogger.Instance.ServerUpdate.LogTime("BaseNetworkable.Kill", ms);
+    }
+}
